64527ac5793709914e723834fee1600c8d1380da
flkt-crnpio <flkt.crnpio@gmail.com>
2018-03-12 14:22:27 -0600

    file-folder structure started

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync').create(),
+    supportedbrowsers = [
+        'last 2 versions',
+        'safari >= 8',
+        'ie >= 10',
+        'ff >= 20',
+        'ios 6',
+        'android 4'
+    ];
+
+gulp.task('dist', ['sass-dist']);
+
+gulp.task('dev', ['normalize', 'grid', 'sass-dev'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dev"
+        }
+    });
+    gulp.watch("dev/index.html").on('change', browserSync.reload);
+    gulp.watch("src/**/*.scss", ['sass-dev']);
+});
+
+gulp.task('sass-dist', function(){
+    return gulp.src(['src/colors.scss'])
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(cssnano({
+            autoprefixer: {browsers: supportedbrowsers, add: true}
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('sass-colors', function(){
+    return gulp.src(['src/colors.scss'])
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('dev/css'));
+});
+
+gulp.task('sass-dev', ['sass-colors'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('normalize', function() {
+    return gulp.src('node_modules/m.normalize/normalize.min.css')
+        .pipe(gulp.dest('dev/css/'));
+});
+gulp.task('grid', function() {
+    return gulp.src('node_modules/m.grid/dist/grid.min.css')
+        .pipe(gulp.dest('dev/css/'));
+});
